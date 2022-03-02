1738966db709c2e179519477acb6e07d79dcaef3
adam.arlotte <adam.arlotte@acftechnologies.com>
2022-03-02 17:19:34 +0000

    Update for testing docker

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace DockerApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Update!");
         }
     }
 }
